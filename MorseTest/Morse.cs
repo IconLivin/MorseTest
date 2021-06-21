a7909682e9ef104c5ed18942d42b7da67536352b
IconLivin <anton.parole@mail.ru>
2021-06-21 10:02:17 +0300

    MorseTest

---
+++ MorseTest/Morse.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseTest
+{
+    class Morse
+    {
+        public static IDictionary<string, string> MorseCodeDictionary =
+            new Dictionary<string, string>() {
+                { ".-", "A"},
+                { "-...", "B"},
+                { "-.-.", "C"},
+                { "-..", "D"},
+                { ".", "E"},
+                { "..-.", "F"},
+                { "--.", "G"},
+                { "....", "H"},
+                { "..", "I"},
+                { ".---", "J"},
+                { "-.-", "K"},
+                { ".-..", "L"},
+                { "--", "M"},
+                { "-.", "N"},
+                { "---", "O"},
+                { ".--.", "P"},
+                { "--.-", "Q"},
+                { ".-.", "R"},
+                { "...", "S"},
+                { "-", "T"},
+                { "..-", "U"},
+                { "...-", "V"},
+                { ".--", "W"},
+                { "-..-", "X"},
+                { "-.--", "Y"},
+                { "--..", "Z"},
+            };
+        public static IDictionary<string, string> LetterToMorseCodeDictionary =
+            new Dictionary<string, string>() {
+                { "A", ".-" },
+                { "B", "-..."},
+                { "C", "-.-."},
+                { "D", "-.."},
+                { "E", "."},
+                { "F", "..-."},
+                { "G", "--."},
+                { "H", "...."},
+                { "I", ".."},
+                { "J", ".---"},
+                { "K", "-.-"},
+                { "L", ".-.."},
+                { "M", "--"},
+                { "N", "-."},
+                { "O", "---"},
+                { "P", ".--."},
+                { "Q", "--.-"},
+                { "R", ".-."},
+                { "S", "..."},
+                { "T", "-"},
+                { "U", "..-"},
+                { "V", "...-" },
+                { "W", ".--"},
+                { "X", "-..-"},
+                { "Y", "-.--"},
+                { "Z", "--.."}
+            };
+
+        public static string MorseDecoder(string encoded_message)
+        {
+            if (encoded_message.Length == 0)
+            {
+                throw new ArgumentException("Empty message is passed");
+            }
+
+            string result = "";
+
+            foreach (string word in encoded_message.Split(new string[] { "   " }, StringSplitOptions.None)){
+                foreach(string letter in word.Split(' '))
+                {
+                    if (letter.Length == 0)
+                    {
+                        throw new ArgumentException("Empty letter is found");
+                    }
+
+                    result = MorseCodeDictionary.ContainsKey(letter) ? result + MorseCodeDictionary[letter] : result + "*";
+                }
+                result += " ";
+            }
+
+            return result.Substring(0, result.Length - 1);
+        }
+
+        public static string MorseEncoder(string to_encode_mesage)
+        {
+            string result = "";
+            foreach (string word in to_encode_mesage.ToUpper().Split(' '))
+            {
+                foreach(char letter in word)
+                {
+                    result = LetterToMorseCodeDictionary.ContainsKey(letter.ToString()) ? result + LetterToMorseCodeDictionary[letter.ToString()] : result + "-----";
+                    result += " ";
+                }
+                result += "  ";
+            }
+            return result.Substring(0,result.Length - 3);
+        }
+    }
+}
---
+++ MorseTest/MorseTest.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseTest
+{
+
+    /// <summary>
+    /// Test Morse code decoder
+    /// </summary>
+    /// 
+    [TestFixture]
+    public class MorseTest
+    {
+        private string SWCM; // Single word correct message
+        private string MWCM; // Multiple words correct message
+        private string ULCM; // Unfined letter correct message
+        private string EM; // Empty message
+        private string OSM; // One space message
+
+        /// <summary>
+        /// Initialize messages
+        /// </summary>
+        [SetUp]
+        protected void SetUp()
+        {
+            SWCM = Morse.MorseEncoder("Hello");
+            MWCM = Morse.MorseEncoder("Hello World");
+            ULCM = Morse.MorseEncoder("Hello") + "   ...--- ...--";
+            EM = "";
+            OSM = " ";
+
+        }
+
+        /// <summary>
+        /// Assert correct single word message decoding
+        /// </summary>
+        /// 
+        [Test]
+        public void Single_Word_Decoder()
+        {
+            string expected = "HELLO";
+            string decoded_message = Morse.MorseDecoder(SWCM);
+            Assert.AreEqual(expected, decoded_message);
+        }
+
+        /// <summary>
+        /// Assert correct multiple words messsage decoding
+        /// </summary>
+        /// 
+        [Test]
+        public void Multiple_Words_Decoder()
+        {
+            string expected = "HELLO WORLD";
+            string decoded_message = Morse.MorseDecoder(MWCM);
+            Assert.AreEqual(expected, decoded_message);
+        }
+
+        /// <summary>
+        /// Assert correct message with unfined letter
+        /// </summary>
+        /// 
+        [Test]
+        public void Unfined_Letter_Decoder()
+        {
+            string expected = "HELLO **";
+            string decoded_message = Morse.MorseDecoder(ULCM);
+            Assert.AreEqual(expected, decoded_message);
+        }
+
+        /// <summary>
+        /// Assert decoder throws an excpetion if empty message is passed
+        /// </summary>
+        /// 
+        [Test]
+        public void Empty_Message_Decoder()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Morse.MorseDecoder(EM));
+            Assert.That(ex.Message == "Empty message is passed");
+        }
+
+        /// <summary>
+        /// Assert decoder throws an exception if empty letter is found
+        /// </summary>
+        /// 
+        [Test]
+        public void Space_Message_Decoder()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Morse.MorseDecoder(OSM));
+            Assert.That(ex.Message == "Empty letter is found");
+        }
+    }
+}
